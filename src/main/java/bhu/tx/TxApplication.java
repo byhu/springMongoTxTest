4922b6a802ba3da367c1ab08ce94cd3159cceab7
fishMightBeHere <57296052+fishMightBeHere@users.noreply.github.com>
2021-06-21 09:04:02 -0400

    test cases

--- src/main/java/bhu/tx/TxApplication.java
+++ src/main/java/bhu/tx/TxApplication.java
@@ -1,8 +1,5 @@
 package bhu.tx;
 
-import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,17 +8,16 @@ import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
 import org.springframework.data.mongodb.SessionSynchronization;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.transaction.reactive.TransactionalOperator;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.data.mongodb.core.query.Criteria.where;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class TxApplication implements CommandLineRunner {
-    Logger log = LoggerFactory.getLogger(getClass());
 
     public static void main(String[] args) {
         SpringApplication.run(TxApplication.class, args);
@@ -29,42 +25,112 @@ public class TxApplication implements CommandLineRunner {
 
     @Autowired
     ReactiveMongoTemplate mongo;
+    //    @Autowired
+//    MongoTemplate imongo;
     @Autowired
     ReactiveMongoDatabaseFactory factory;
 
-    @Override
-    public void run(java.lang.String... args)  {
-        mongo.remove(Query.query(where("_id").exists(true)), A.class).block();
-        mongo.remove(Query.query(where("_id").exists(true)), B.class).block();
+    @java.lang.Override
+    public void run(java.lang.String... args) throws Exception {
+        mongo.remove(A1.class).all().block();
+        mongo.remove(A2.class).all().block();
 
+        // session synchronization doesn't seem to make any difference
         mongo.setSessionSynchronization(SessionSynchronization.ALWAYS);
+
         var txo = TransactionalOperator.create(new ReactiveMongoTransactionManager(factory), new DefaultTransactionDefinition());
 
+        // test cases
+//        flux(txo);      // failed
+//        monoZip(txo); // failed
+//        fluxO(txo);   // failed, if you run multiple times, you will see 1 or 2 records of A1 inserted.
+
+//        recursive(txo, 0).block(); // success
+        mono(txo);        // success
+//        monoConcat(txo);  // success
+    }
+
+    private Mono<A2> save(int i) {
+        var d0 = new A1(i, "asdf " + i);
+        var d1 = new A2(i, "asdfadf " + i);
+        return mongo.save(d0)
+                .flatMap(it -> {
+                    if (i % 20 == 19)
+                        return Mono.error(new Exception("ddd"));
+                    return Mono.empty();
+                })
+                .then(mongo.save(d1));
+    }
+
+    private Mono<Void> recursive(TransactionalOperator txo, int i) {
+        if (i > 1000)
+            return Mono.empty();
+        return txo.transactional(save(i))
+                .onErrorResume(e -> Mono.empty())
+                .then(recursive(txo, i + 1));
+    }
+
+    private void flux(TransactionalOperator txo) {
         Flux.range(0, 1000)
-                .flatMap(i ->
-                    txo.transactional(mongo.save(new A(i))
-                            .flatMap(it -> i % 30 == 29 ? Mono.error(new Exception("")) : Mono.empty())
-                            .concatWith(mongo.save(new B(i))))
-                )
-                .subscribe();
+                .flatMap(i -> txo.transactional(save(i)))
+                .onErrorResume(e -> Mono.empty())
+                .blockLast();
+    }
+
+    private void fluxO(TransactionalOperator txo) {
+        txo.transactional(Flux.range(0, 1000)
+                .flatMap(this::save))
+                .blockLast();
+    }
+
+    private void mono(TransactionalOperator txo) {
+        var l = IntStream.range(1, 1000).mapToObj(i -> txo.transactional(save(i))).collect(Collectors.toList());
+        l.stream().reduce((a, b) -> a.then(b))
+                .get()
+                .block();
+    }
+
+    private void monoConcat(TransactionalOperator txo) {
+        var  m = txo.transactional(save(0)).concatWith(txo.transactional(save(1)));
+        for (int i = 2; i < 100; i++)
+            m = m.concatWith(txo.transactional(save(i)));
+        m.onErrorContinue((e, a) -> {})
+                .blockLast();
+    }
+
+    private void monoZip(TransactionalOperator txo) {
+        var l = IntStream.range(1, 1000).mapToObj(i -> txo.transactional(save(i))).collect(Collectors.toList());
+        Mono.zip(l, a -> a)
+                .onErrorResume(e -> Mono.empty())
+                .block();
+        System.exit(0);
     }
 }
 
-class A {
-    public String id = ObjectId.get().toHexString();
-    public Integer i;
+class A1 {
+    public int id;
 
-    public A(Integer i) {
-        this.i = i;
+    public A1() {
     }
+
+    public A1(int id, String data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public String data;
 }
 
+class A2 {
+    public int id;
 
-class B {
-    public String id = ObjectId.get().toHexString();
-    public Integer i;
-
-    public B(Integer i) {
-        this.i = i;
+    public A2() {
     }
+
+    public A2(int id, String data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public String data;
 }
