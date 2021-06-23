f5e986811be7c314ef5c9ab4db964a126f121873
fishMightBeHere <57296052+fishMightBeHere@users.noreply.github.com>
2021-06-23 13:04:44 -0400

    narrow down

--- src/test/java/bhu/tx/TxTest.java
+++ src/test/java/bhu/tx/TxTest.java
@@ -32,17 +32,15 @@ public class TxTest {
                 .block();
     }
 
-    Mono<Void> save(int i, String col) {
-        var d0 = new A(i, "a1 " + i);
-        var d1 = new A(i, "a2 " + i);
-        return mongo.save(d0, col + "0")
+    Mono<A> save(int i, String col) {
+        var a = new A(i, "a1 " + i);
+        return mongo.save(a, col + "0")
                 .flatMap(it -> {
                     if (i % 30 == 29)
                         return Mono.error(new Exception("Out"));
                     return Mono.empty();
                 })
-                .then(mongo.save(d1, col + "1"))
-                .then();
+                .then(mongo.save(a, col + "1"));
     }
 
     private Mono<Void> countEqual(String col) {
@@ -59,29 +57,12 @@ public class TxTest {
     public void flux() {
         cleanup("flux");
         try {
-            Flux.range(0, 100)
+            Flux.range(0, 1000)
                     .flatMap(i -> txo.transactional(save(i, "flux")))
-//                    .then()
-//                .onErrorResume(e -> Mono.empty())
                     .then(countEqual("flux")).block();
         } catch (Exception e) {
-            Mono.delay(Duration.ofSeconds(2))
-                    .then(countEqual("flux"))
-                    .block();
-        }
-    }
-
-    @Test
-    public void fluxSingleTx() {
-        cleanup("fluxSingleTx");
-        try {
-            txo.transactional(Flux.range(0, 100)
-                    .flatMap(i -> save(i, "fluxSingleTx")))
-                    .then()
-//                .onErrorResume(e -> Mono.empty())
-                    .then(countEqual("fluxSingleTx")).block();
-        } catch (Exception e) {
-            countEqual("fluxSingleTx").block();
+//            System.exit(1);
+            countEqual("flux").block();
         }
     }
 
@@ -89,71 +70,81 @@ public class TxTest {
     public void monoZip() {
         cleanup("monoZip");
         try {
-            var l = IntStream.range(1, 100).mapToObj(i -> txo.transactional(save(i, "monoZip"))).collect(Collectors.toList());
+            var l = IntStream.range(1, 1000).mapToObj(i -> txo.transactional(save(i, "monoZip"))).collect(Collectors.toList());
 
             Mono.zip(l, a -> a)
                     .then()
-//                .onErrorResume(e -> Mono.empty())
                     .then(countEqual("monoZip")).block();
         } catch (Exception e) {
+//            throw e;
             countEqual("monoZip").block();
         }
     }
 
-    @Test
-    public void mono() {
-        cleanup("mono");
-        try {
-            var l =
-                    IntStream.range(1, 100).mapToObj(i -> txo.transactional(save(i, "mono"))).collect(Collectors.toList());
-
-            l.stream().reduce(Mono::then)
-                    .get()
-                    .then()
-//                .onErrorResume(e -> Mono.empty())
-                    .then(countEqual("mono")).block();
-        } catch (Exception e) {
-            countEqual("mono").block();
-        }
-    }
-
-    @Test
-    public void monoConcat() {
-        cleanup("monoConcat");
-        try {
-            var m =
-                    txo.transactional(save(0, "monoConcat"))
-                            .concatWith(txo.transactional(save(1, "monoConcat")));
-
-            for (int i = 2; i < 100; i++)
-                m = m.concatWith(txo.transactional(save(i, "monoConcat")));
-
-            m.then()
-//                    .onErrorResume(e -> Mono.empty())
-                    .then(countEqual("monoConcat")).block();
-        } catch (Exception e) {
-            countEqual("monoConcat").block();
-        }
-    }
-
-    Mono<Void> recursive(int i) {
-        if (i > 100)
-            return Mono.empty();
-        return txo.transactional(save(i, "recursive"))
-                .then(recursive(i + 1));
-    }
-
-    @Test
-    public void recursive() {
-        cleanup("recursive");
-        try {
-            recursive(100)
-//                    .onErrorResume(e -> Mono.empty())
-                    .then(countEqual("recursive")).block();
-        } catch (Exception e) {
-            countEqual("recursive").block();
-        }
-    }
+//    @Test
+//    public void fluxSingleTx() {
+//        cleanup("fluxSingleTx");
+//        try {
+//            txo.transactional(Flux.range(0, 1000)
+//                    .flatMap(i -> save(i, "fluxSingleTx")))
+//                    .then()
+//                    .then(countEqual("fluxSingleTx")).block();
+//        } catch (Exception e) {
+//            countEqual("fluxSingleTx").block();
+//        }
+//    }
+//
+//
+//    @Test
+//    public void mono() {
+//        cleanup("mono");
+//        try {
+//            var l =
+//                    IntStream.range(1, 1000).mapToObj(i -> txo.transactional(save(i, "mono"))).collect(Collectors.toList());
+//
+//            l.stream().reduce(Mono::then)
+//                    .get()
+//                    .then()
+//                    .then(countEqual("mono")).block();
+//        } catch (Exception e) {
+//            countEqual("mono").block();
+//        }
+//    }
+//
+//    @Test
+//    public void monoConcat() {
+//        cleanup("monoConcat");
+//        try {
+//            var m =
+//                    txo.transactional(save(0, "monoConcat"))
+//                            .concatWith(txo.transactional(save(1, "monoConcat")));
+//
+//            for (int i = 2; i < 1000; i++)
+//                m = m.concatWith(txo.transactional(save(i, "monoConcat")));
+//
+//            m.then(countEqual("monoConcat")).block();
+//        } catch (Exception e) {
+//            countEqual("monoConcat").block();
+//        }
+//    }
+//
+//    Mono<Void> recursive(int i) {
+//        if (i >= 1000)
+//            return Mono.empty();
+//        return txo.transactional(save(i, "recursive"))
+//                .then(recursive(i + 1));
+//    }
+//
+//    @Test
+//    public void recursive() {
+//        cleanup("recursive");
+//        try {
+//            recursive(0)
+//                    .then(countEqual("recursive")).block();
+//        } catch (Exception e) {
+//            countEqual("recursive").block();
+//        }
+//    }
 }
 
 class A {
